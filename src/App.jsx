5298973f753b299ebeec1dcf34203096d2e27dff
Mirabbosbek9778 <jwicj969@gmail.com>
2023-08-03 15:01:48 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import Main from './components/Main';
 import Test from "./Test";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
